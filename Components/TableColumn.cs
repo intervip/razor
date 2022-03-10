5d498f9c6052ef64ed8e961e5f12b697309b923b
Bruno Blanes <bruno.blanes@outlook.com>
2022-03-10 16:25:03 -0300

    Cleaned up the `Table` implementation (#22)

---
+++ Components/TableColumn.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System.Linq.Expressions;
+
+namespace Intervip.Razor.Components;
+
+public partial class TableColumn<TItem> : ComponentBase
+{
+	private Func<TItem, object>? compiledExpression;
+
+	[CascadingParameter]
+	public Table<TItem>? ParentTable { get; set; }
+
+	[Parameter, EditorRequired]
+	public Expression<Func<TItem, object>?>? Expression { get; set; }
+
+	protected override void OnInitialized()
+	{
+		ParentTable?.AddTableRow(this);
+		base.OnInitialized();
+	}
+
+	protected override void OnParametersSet()
+	{
+		compiledExpression = Expression?.Compile();
+		base.OnParametersSet();
+	}
+
+	internal object GetCellValue(TItem item)
+	{
+		ArgumentNullException.ThrowIfNull(compiledExpression);
+		return compiledExpression(item);
+	}
+}
