946a355b91b93d75d05eadd44b09a71c8de09295
Bruno Blanes <bruno.blanes@outlook.com>
2022-04-18 23:03:42 -0300

    Code refactoring and cleanup

---
+++ EventArguments/ItemClickedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+using System.Reflection;
+
+namespace Intervip.Razor.Components;
+public class ItemClickedEventArgs<T> : MouseEventArgs
+{
+	public T? Item { get; set; }
+
+	public ItemClickedEventArgs(MouseEventArgs args) : base()
+	{
+		foreach (PropertyInfo? property in typeof(MouseEventArgs).GetProperties())
+		{
+			property.SetValue(this, property.GetValue(args, null), null);
+		}
+	}
+}
